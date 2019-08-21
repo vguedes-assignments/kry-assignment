5f18c5343645296e5a3444b879c52b5a8c606182
VincentGuedes <vincentcguedes@gmail.com>
2019-08-21 16:53:06 +0200

    implement poller and routes

--- src/main/java/se/kry/codetest/BackgroundPoller.java
+++ src/main/java/se/kry/codetest/BackgroundPoller.java
@@ -5,11 +5,29 @@ import io.vertx.core.Future;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class BackgroundPoller {
 
-  public Future<List<String>> pollServices(Map<String, String> services) {
-    //TODO
-    return Future.failedFuture("TODO");
+
+  public Future<List<String>> pollServices(Map<String, Service> services) {
+    System.out.println(services);
+    services.forEach((key, value) -> {
+      try {
+        HttpURLConnection httpConnection = (HttpURLConnection) new URL(key).openConnection();
+        httpConnection.setRequestMethod("GET"); 
+        httpConnection.setConnectTimeout(5000);
+        int responseCode = httpConnection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+          services.get(key).setStatus(Service.Status.OK);
+        } else {
+          services.get(key).setStatus(Service.Status.FAIL);
+        }
+      } catch (Exception e) {
+        Future.failedFuture(e.getMessage());
+      }
+    });
+    return Future.succeededFuture();
   }
 }
